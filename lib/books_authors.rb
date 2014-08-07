213da5e9cd53261d3d004d3daf7759319a93a3f4
Sean Morrison & Nuatu Tseggai <student@epicodus.com>
2014-08-07 16:59:24 -0700

    Returns authors for a given book id.

--- lib/books_authors.rb
+++ lib/books_authors.rb
@@ -1,5 +1,17 @@
+
+
 def add_book_and_author(book_id, author_id)
   DB.exec("INSERT INTO books_authors (book_id,author_id) VALUES (#{book_id}, #{author_id});")
 end
 
+
+def author_search(book_id)
+  output = []
+  results = DB.exec("SELECT * FROM books_authors JOIN authors ON (books_authors.author_id = authors.id) WHERE book_id = #{book_id};")
+  # count = DB.exec("SELECT COUNT(*) FROM books_authors JOIN authors ON (books_authors.author_id = authors.id) WHERE book_id = #{book_id};").first['count'].to_i
+  results.each do |result|
+    output << result['name']
+  end
+p output
+end
 
--- spec/books_authors_spec.rb
+++ spec/books_authors_spec.rb
@@ -15,6 +15,30 @@ describe 'add_book_and_author' do
     add_book_and_author(test_book2.id,test_author2.id)
     expect(DB.exec('SELECT COUNT(*) FROM books_authors;').first['count']).to eq "2"
   end
+  it 'returns all authors for a given book id' do
+    test_book = Book.new({'title'=> 'The Gas'})
+    test_book.save
+    test_book2 = Book.new(ATTRIBUTES)
+    test_book2.save
+    test_author = Author.new({'name'=>'Fred Barnes'})
+    test_author.save
+    test_author2 = Author.new({'name'=> 'Tap Root'})
+    test_author2.save
+    test_author3 = Author.new({'name'=> 'Battle Bailey'})
+    test_author3.save
+    test_author4 = Author.new(ATTRIBUTES)
+    test_author4.save
+    test_author5 = Author.new(ATTRIBUTES)
+    test_author5.save
+    add_book_and_author(test_book.id,test_author.id)
+    add_book_and_author(test_book.id,test_author2.id)
+    add_book_and_author(test_book.id,test_author3.id)
+    add_book_and_author(test_book2.id,test_author.id)
+    add_book_and_author(test_book2.id,test_author4.id)
+    add_book_and_author(test_book2.id,test_author5.id)
+    expect(author_search(test_book.id)).to eq ["Fred Barnes", "Tap Root", "Battle Bailey"]
+  end
+
 end
 
-    # DB.exec("SELECT COUNT(*)FROM books_authors;").first['count']
+
